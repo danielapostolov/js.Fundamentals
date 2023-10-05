1906e4e923cb104079d2595cf8de907da14db9eb
danielapostolov <d.kaloyanoff@gmail.com>
2023-10-05 23:04:19 +0300

    Exc.2 new solution

--- 03-arrays-excercise/02-commonElements.js
+++ 03-arrays-excercise/02-commonElements.js
@@ -1,22 +1,9 @@
 function commonElements(arr1, arr2) {
 
 
-    for (let index = 0; index < arr1.length; index++) {
-        let firstElement = arr1[index];
-
-
-
-        let index2 = 0;
-        let secondElement = arr2[index2];
-        while (index2 < arr2.length) {
-
-
-
-            if (firstElement === secondElement) {
-                console.log(firstElement);
-            }
-            index2++;
-            secondElement = arr2[index2];
+    for (let element of arr1){
+        if(arr2.includes(element)){
+            console.log(element);
         }
     }
 
