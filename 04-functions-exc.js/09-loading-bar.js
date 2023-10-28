2580b3f5eed54079bacb7db7d7afd1c689df3814
danielapostolov <d.kaloyanoff@gmail.com>
2023-10-28 16:04:54 +0300

    arr

--- 04-functions-exc.js/09-loading-bar.js
+++ 04-functions-exc.js/09-loading-bar.js
@@ -1,16 +1,15 @@
 function loadingBar(num) {
-    let percentsCount = num/10;
+    let percentsCount = num / 10;
     let percents = ('%').repeat(percentsCount);
     let points = '.'.repeat(10 - percentsCount);
 
-    if (num != 100){
+    if (num != 100) {
         console.log(`${num}% [${percents}${points}]`);
         console.log('Still loading...');
-    }else{
+    } else {
         console.log('100% Complete!');
         console.log('[%%%%%%%%%%]');
     }
-
 
 }
 loadingBar(30)
---
+++ 05-arrays-advanced-exc/04-sorting.js
@@ -0,0 +1,16 @@
+function sorting(arr) {
+    let sortedArr = arr.sort((a, b) => a - b);
+    let finalArr = [];
+
+    while (sortedArr.length > 0) {
+        let maxNum = sortedArr.pop();
+        finalArr.push(maxNum);
+        if(sortedArr.length > 0){
+            let minNum = sortedArr.shift();
+            finalArr.push(minNum);
+        }
+    }
+
+    console.log(finalArr.join(' '));
+}
+sorting([1, 21, 3, 52, 69, 63, 31, 2, 18, 94])
